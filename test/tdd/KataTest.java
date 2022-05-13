72d3e9c81a64fc723e8ae1585be2c683abe63f63
Nouah <noahgreatakoni@gmail.com>
2022-05-13 17:05:32 +0100

    new files

--- test/tdd/KataTest.java
+++ test/tdd/KataTest.java
@@ -4,55 +4,73 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KataTest {
 
     @Test
 
-    public void additionTest(){
+    public void additionTest() {
         Kata calculator = new Kata();
-        int result =  calculator.add(2,6);
+        int result = calculator.add(2, 6);
         assertEquals(8, result);
-        assertEquals(66, calculator.add(54,12));
+        assertEquals(66, calculator.add(54, 12));
     }
 
     @Test
-    public void substractionTest(){
+    public void substractionTest() {
         Kata calculator = new Kata();
-        int result = calculator.substract(4,2);
+        int result = calculator.substract(4, 2);
         assertEquals(2, result);
 
     }
+
     @Test
-    public void subTest(){
-     Kata calculator = new Kata();
-     int o= calculator.substract(2,4);
-     assertEquals(-2,o);
+    public void subTest() {
+        Kata calculator = new Kata();
+        int o = calculator.substract(2, 4);
+        assertEquals(-2, o);
 
     }
+
     @Test
-    public void productTest(){
+    public void productTest() {
         Kata calculator = new Kata();
-        int m = calculator.product(6,2);
+        int m = calculator.product(6, 2);
         assertEquals(12, m);
     }
+
     @Test
-    public void quotientTest(){
+    public void quotientTest() {
         Kata calculator = new Kata();
-        int k = calculator.quotient(10,0);
-        assertEquals(0,k);
+        int k = calculator.quotient(10, 0);
+        assertEquals(0, k);
     }
 
     @Test
-    public void gradeTest(){
+    public void gradeTest() {
 
         Kata calculator = new Kata();
         String grade = calculator.gradeScore(55);
-        assertEquals( "F", grade);
+        assertEquals("F", grade);
     }
 
+    @Test
+
+    public void numberFactorTest(){
+        int factors = Kata.noOfFactorsOf(5);
+        assertEquals(2, factors);
+
+        factors =Kata.noOfFactorsOf(10);
+        assertEquals(4, factors);
 
     }
+    @Test
+    public void primeNumberTest(){
+        boolean isPrime= Kata.isPrime(5);
+        assertTrue(isPrime);
+    }
+}
 
 
 
