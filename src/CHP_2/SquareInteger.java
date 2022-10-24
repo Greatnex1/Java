862c391beb73dcb3c9410db08db97baec8f2315b
Nouah <noahgreatakoni@gmail.com>
2022-10-24 21:27:07 +0100

    working code

---
+++ src/CHP_2/SquareInteger.java
@@ -0,0 +1,20 @@
+package CHP_2;
+
+import java.util.Scanner;
+
+public class SquareInteger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int square = scan.nextInt();
+    int result =    doubleNumber(square);
+        System.out.println(result);
+    }
+    private static int doubleNumber(int number){
+       int answer =  number * number;
+       if (answer > 100){
+           System.out.println("the square of this number is greater than 100" );
+       }
+           return answer;
+    }
+}
