b7c4d1d1ce08051e4eb2b7c6da8611fdabf49368
Nouah <noahgreatakoni@gmail.com>
2022-11-26 22:44:40 +0100

    working code

--- src/CHP_2/SquareInteger.java
+++ src/CHP_2/SquareInteger.java
@@ -3,6 +3,7 @@ package CHP_2;
 import java.util.Scanner;
 
 public class SquareInteger {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a number");
