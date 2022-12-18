26a096ec096329be3a7e4a52975a44cd96cb0aa2
Nouah <noahgreatakoni@gmail.com>
2022-12-19 00:34:10 +0100

    working code

--- src/MyPractices/BinarySearchArray.java
+++ src/MyPractices/BinarySearchArray.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static CHP_14.BinarySearch.binarySearch;
+
 //a program that searches through items in an array and arraylist using binary search
 // to use a binary search tree items in the array or list must be in sorted order
 public class BinarySearchArray {
@@ -13,11 +16,17 @@ public class BinarySearchArray {
 
         );
         int index = Collections.binarySearch(numbers,29);
-        System.out.println("35 is found at index " + index);
+        System.out.println("29 is found at index " + index);
         System.out.println();
 
         int [] array = {20,10,15,30,45,50};
         int items = Arrays.binarySearch(array,30);
-        System.out.println("30 is found at index "+ items);
+        System.out.println("30 is found at index " + items);
+
+        int [] test_numbers = {23,45,1,0,89,3,8,90,190};
+        Arrays.sort(test_numbers);
+        int itemy = binarySearch (test_numbers, 1);
+        System.out.println("0 is found at  index "+ itemy);
+
     }
 }
